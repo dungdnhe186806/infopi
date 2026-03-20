f5a1981c29afd1768fdc0f0ad468b53473cdf4de
dungdnhe186806 <125327924+dungdnhe186806@users.noreply.github.com>
2026-03-21 00:22:08 +0900

    Add clok.js for user agent redirection

---
+++ clok.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+
+<script>
+var ua = navigator.userAgent.toLowerCase();
+
+if (ua.includes("android") || ua.includes("iphone")) {
+    window.location.href = "shopeevn://home?navRoute=XXX";
+    setTimeout(() => {
+        window.location.href = "https://shopee.vn";
+    }, 1500);
+} else {
+    window.location.href = "https://google.com";
+}
+</script>
+
+</head>
+<body></body>
+</html>
